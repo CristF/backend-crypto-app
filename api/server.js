e24fbca84bab800a7d240ac463bad96548908558
CristF <156393350+CristF@users.noreply.github.com>
2025-03-27 23:21:24 -0400

    Update file names

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
@@ -12,12 +13,21 @@ app.use(cors())
 app.use(helmet())
 app.use(express.json())
 
+// MongoDB Connection
+mongoose.connect(process.env.URI)
+  .then(() => {
+    console.log('MongoDB Connected Successfully')
+    console.log(`Mongoose Version: ${mongoose.version}`)
+  })
+  .catch(err => console.error('MongoDB connection error:', err))
+
+sample_mflix.user.find()
 // Basic test route
 app.get('/', (req, res) => {
   res.json({ message: 'Crypto Tracker API is running' })
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
